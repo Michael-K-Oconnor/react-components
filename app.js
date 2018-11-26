c6c6101927d545fd5d3ceee78f5cf593d3da1321
michael-k-oconnor <m.k.oconnor53@gmail.com>
2018-11-26 15:10:38 -0600

    Render groceries

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// TODO
+let DisplayGroceryList = (props) => (
+  <div>
+    <Groceries list={props.list}/>
+  </div>
+);
+
+let Groceries = (props) => (
+  <ul>
+    <li>{props.list[0]}</li>
+    <li>{props.list[1]}</li>
+  </ul>
+);
+
+ReactDOM.render(<DisplayGroceryList list={["cucumbers", "kale"]} />, document.getElementById("app"));
+/*  */
